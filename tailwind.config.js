f375726be23c0ef1e9a0581e768e1c42368d3955
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2020-11-19 16:27:30 -0500

    Creación de un banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
       colors: {
         'primary': '#f3c614',
         'secondary': '#353535'
+      },
+      height: {
+        '80': '20rem',
+        '100': '25rem',
+        '120': '30rem'
       }
     },
   },
