d24f0b756a1226c98f192788a676abe38498582f
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-11-23 16:44:07 -0500

    chore: indent with two spaces

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        purgecss({
-            content: ['./**/*.html'],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        }),
-        require('cssnano')({
-            preset: 'default',
-        }),
-    ]
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    purgecss({
+      content: ['./**/*.html'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ]
 }
